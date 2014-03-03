f73e7ba3ac85319535b1a1c09c15a4cc533766a3
chetan-palavalli <chetan.palavalli@gmail.com>
2014-03-03 20:33:13 +0530

    commiting

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from lists.views import home_page
 from django.core.urlresolvers import resolve
 from django.http import HttpResponse
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -9,8 +11,22 @@ class SmokeTest(TestCase):
         self.assertEqual(found.func,home_page)
     
     def test_home_page_returns_correct_html(self):
-        request = HttpResponse()
+        request = HttpRequest()
         response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+        self.assertIn('A new list item', response.content.decode())
