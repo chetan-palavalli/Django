b8abaa539c661b22280b3b61e53876d887a49b26
chetan-palavalli <chetan.palavalli@gmail.com>
2014-03-27 20:57:32 +0530

    1)Bug fix in home.html 2)URLs for forms x2, moved code in views + tests, new URL

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,10 +4,6 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
     #items = Item.objects.all()
     #returnDict = {'items': items}
@@ -15,3 +11,6 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
